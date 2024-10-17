9f3796f0819a08a5dc1ed639f86aa095fd7c878b
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2024-10-17 17:53:59 -0300

    Conexión base de datos

---
+++ index.js
@@ -0,0 +1,20 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import db from "./config/db.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+//app.use("/api")
+
+const PORT = process.env.PORT || 3001;
+
+db.sync({ force: false }).then(() => {
+  console.log("DB conectada");
+  app.listen(PORT, () => console.log(`Server escuchando en el puerto ${PORT}`));
+});
